4605512ab93c2f80279e3e502fe6062ca270580a
Leonardo Cardoso <lcard00@outlook.com>
2024-02-29 14:01:28 -0300

    style: :art: formatar documento

--- Python Para a Engenharia de Dados/Módulo 01/Sequencial.py
+++ Python Para a Engenharia de Dados/Módulo 01/Sequencial.py
@@ -4,15 +4,16 @@ from os import system, name
 import numpy
 import math
 
+
 def msg_aguardar():
     print("\nPressione qualquer tecla para continuar.")
     input()
-    
+
     if name == "nt":
         system("CLS")
     else:
         system("clear")
-        
+
 
 menu = """
     Informe qual o exercicio que resolver:
@@ -29,21 +30,21 @@ menu = """
 => """
 
 while True:
-    op  = input(menu)        
-    
+    op = input(menu)
+
     print("")
-    
+
     match op:
         case "1":
             print("Alo mundo")
             msg_aguardar()
-            
+
         case "2":
             numero = int(input("Informe um número inteiro: "))
 
             print(f"\nO número informado foi {numero}")
-            msg_aguardar() 
-        
+            msg_aguardar()
+
         case "3":
             numero_1 = int(input("Informe o primeiro número inteiro: "))
             numero_2 = int(input("Informe o segundo número inteiro: "))
@@ -62,22 +63,22 @@ while True:
 
             print(f"\nA média das notas é: {notas_avg}")
             msg_aguardar()
-        
+
         case "5":
             m = float(input("Informe um número em metros: "))
-            
+
             print(f"\n{m} m em cm é equivalente a {m*100} cm.")
             msg_aguardar()
-        
+
         case "6":
             diametro = float(input("Informe o diametro do circulo (cm): "))
-            
+
             r = diametro / 2
-            area = math.pi * r ** 2
-            
+            area = math.pi * r**2
+
             print(f"os dados do circulo informado é: \n\n diametro - {diametro} cm \n raio - {r} cm \n area - {area:.2f} cm2 \n")
             msg_aguardar()
-            
+
         case "q":
             print("Saindo...")
             break
