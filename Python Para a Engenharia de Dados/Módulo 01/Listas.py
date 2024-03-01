cf81df9836c2015a80a144f50fc9b9869b18b61e
Leonardo Cardoso <lcard00@outlook.com>
2024-03-01 16:09:22 -0300

    feat: Adiciona listas

---
+++ Python Para a Engenharia de Dados/Módulo 01/Listas.py
@@ -0,0 +1,36 @@
+lista = ["João", "Ana", "Paulo", "Mateus", "Nasser"]
+
+# print(lista[-1])
+
+# print(lista[1:])
+
+# print(lista[1:3])
+
+# print(lista[:-1])
+
+# print(lista[::-1])
+
+# print(lista[::2])
+
+# print(lista)
+
+# lista.append("Leo")
+
+# print(lista)
+
+# lista_2 = lista.copy()
+
+# lista_2.append("Cardoso")
+
+# print(lista)
+# print(lista_2)
+
+# lista.remove("Nasser")
+
+# print(lista)
+
+# lista_3 = lista.copy()
+# lista_3.reverse()
+
+# print(lista)
+# print(lista_3)
