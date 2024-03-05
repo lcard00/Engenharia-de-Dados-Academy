6ff3baf381c080af353b8c78de5da1f5f9021425
Leonardo Cardoso <lcard00@outlook.com>
2024-03-05 13:12:29 -0300

    feat: Add tuplas

---
+++ Python Para a Engenharia de Dados/Módulo 01/tuplas.py
@@ -0,0 +1,21 @@
+tupla = (1,2,3)
+
+tup  = tupla
+
+tup is tupla
+
+tup == tupla
+
+tup != tupla
+
+tupla[0]
+
+# print(tupla[-1])
+
+# print(1 in tupla)
+
+# for item in tupla:
+#     print(item)
+
+len(tupla)
+
