08d010b3ac24fe90c4933f09d2b853fdc91fb0c8
Leonardo Cardoso <lcard00@outlook.com>
2024-03-11 17:09:20 -0300

    Estrutura de pastas

---
+++ python_para_a_engenharia_de_dados/modulo_02/bibliotecas.py
@@ -0,0 +1,23 @@
+import geometria.areas as a
+
+# def area_quadrado(lado):
+#     return lado ** 2
+
+# def area_triangulo(base, altura):
+#     return (base * altura) / 2
+
+# def area_retangulo(base, altura):
+#    return base * altura 
+
+
+num = a.area_quadrado(2)
+
+print(f"Area quadrado: {num}")
+
+num = a.area_triangulo(4, 2)
+
+print(f"Area triangulo: {num}")
+
+num = a.area_retangulo(4,2)
+
+print(f"Area retangulo: {num}")
---
+++ python_para_a_engenharia_de_dados/modulo_02/geometria/areas.py
@@ -0,0 +1,8 @@
+def area_quadrado(lado):
+    return lado ** 2
+
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+def area_retangulo(base, altura):
+   return base * altura 
