693af305a109a0eff5077d5403d1584108148a8a
Leonardo Cardoso <lcard00@outlook.com>
2024-03-12 14:21:34 -0300

    feat: Add bibliotecas internas

---
+++ python_para_a_engenharia_de_dados/modulo_02/bibliotecas_internas.py
@@ -0,0 +1,69 @@
+import functools
+import getpass
+import glob
+import itertools
+import os
+import pprint
+import random
+import string
+import math
+import time
+
+# data = {
+#     'a': 2,
+#     'b': 3,
+#     'c': 4
+# }
+
+# print(data)
+
+# pprint.pprint(data)
+
+# x = "2"
+
+# if x in string.digits:
+#     print(x)
+
+
+# print(math.pi)
+# print(math.sqrt(4))
+
+# print(random.randint(1,6))
+
+# serieA = ["time1", "time2"]
+# serieB = ["time3", "time4"]
+
+# jogos = list(itertools.product(serieA, serieB))
+# print(jogos)
+
+# def soma(a, b):
+#     return a + b
+
+# lista= [1, 2, 3, 4, 5, 5]
+# # sum(lista)
+
+# nova_lista = functools.reduce(soma, lista)
+
+# print(nova_lista)
+
+# path = os.path.join('home', 'alunos.txt')
+
+# print(path)
+
+# current_path = os.getcwd()
+
+# path = os.path.join(current_path, "python_para_a_engenharia_de_dados", "modulo_02")
+
+# files = glob.glob(f'{path}/*.py')
+
+# print(files)
+
+# print(os.name)
+
+# print("olá")
+# time.sleep(5)
+# print("mundo")
+
+# senha = getpass.getpass("Insira sua senha: ")
+
+# print(senha)
