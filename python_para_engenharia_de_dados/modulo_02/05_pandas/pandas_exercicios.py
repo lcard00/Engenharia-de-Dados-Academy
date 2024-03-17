ab4241ebcf74f8d403069e55b9401a25141d0c95
Leonardo Cardoso <lcard00@outlook.com>
2024-03-16 22:45:11 -0300

    Add exercicios pandas 3 parcial

--- python_para_engenharia_de_dados/modulo_02/05_pandas/pandas_exercicios.py
+++ python_para_engenharia_de_dados/modulo_02/05_pandas/pandas_exercicios.py
@@ -1,6 +1,7 @@
-from tracemalloc import start
 import pandas as pd
 import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 np.random.seed(0)  # Para garantir que os resultados sejam reproduzíveis.
 
@@ -62,7 +63,6 @@ df = pd.DataFrame(
 
 # 2. Utilizando pandas, realizze as seguintes alterações no dataset: (semente 0)
 #   1. Transforme 20% das notas em valorres nulos, simulando alunos que não compareceram à prova,.
-
 mask_sample = df.sample(frac=0.2).index
 df.loc[mask_sample, "grade"] = np.nan
 
@@ -77,6 +77,7 @@ df.loc[mask_nan, "grade"] = 0.00
 #         automática do sistema para alunos com idades incosistentes.
 mask_age = df["age"].between(18, 80)
 df.drop(df[~mask_age].index, inplace=True)
+
 # print(df.info())
 
 #   4. Crie um novo campo de aprovado para os alunos restantes que obtiveram nota igual ou superior
@@ -96,9 +97,100 @@ df["weekday"] = weekday
 
 # 3. Gere um relatório com os seguintes tópicos:
 #   1. Tabela cruzada de participantes de cada sexo por estado.
-#   2. Gráffico de pizza da quantidade de aprovados por sexo e um de barras com a quantidade de aprovados por estado
+ct_state_sex = pd.crosstab(df["state"], df["sex"])
+
+# print(ct_state_sex)
+
+#   2. Gráfico de pizza da quantidade de aprovados por sexo e um de barras com a quantidade 
+#       de aprovados por estado
+
+mask_approved = df["approved"] == True
+
+approved_by_sex = df[mask_approved].groupby("sex")["approved"].count()
+
+plt.figure(figsize=(8, 8))
+plt.pie(
+    approved_by_sex,
+    explode = [0.01, 0.01],
+    labels = approved_by_sex.index,
+    autopct = "%.2f%%",
+    # shadow = True,
+    startangle = 90.0,
+    colors = ["tomato", "cornflowerblue"]
+
+)
+
+plt.title(
+    label="Approved by gender", 
+    fontdict={"fontsize":16}
+)
+
+plt.show()
+
+approved_by_state = df[mask_approved].groupby("state")["approved"].count()
+plt.figure(figsize=(14, 10))
+
+plt.barh(
+    approved_by_state.index,
+    approved_by_state,
+    color = "cornflowerblue",
+    alpha=0.9,
+    edgecolor="black",
+    linewidth=0.6,
+    
+)
+
+plt.grid(
+    axis = "x", 
+    alpha = 0.25
+)
+
+plt.ylabel("State")
+
+plt.title(
+    label="Approved by state", 
+    fontdict={"fontsize":16}
+)
+
+plt.show()
+
+
 #   3. Gráfico de pontos de uma nota por idade, colorindo por sexo.
-#         1. Gráfico de barras com a participação por dia da semana e por dia do mês.add()
+mask_grade_by_age = df.groupby(["age", "sex"])["grade"].mean().round(2).reset_index()
+
+mask_grade_by_age = mask_grade_by_age.rename(columns= {"grade" : "mean_grade"})
+
+plt.figure(figsize=(10, 6))
+
+sns.scatterplot(
+    data=mask_grade_by_age, 
+    x="age", 
+    y="mean_grade", 
+    hue="sex", 
+    palette="Set1",
+    alpha = 0.6
+
+)
+
+plt.title("Average grades by age and gender")
+
+plt.xlabel("Age", labelpad = 5)
+
+plt.xticks(np.arange(20, 81, 5))
+
+plt.ylabel("Grade average")
+
+plt.yticks(np.arange(0, 951, 100))
+
+plt.ylim(-50, 950)
+
+plt.legend(title="Sex")
+
+plt.grid(True, alpha=0.2)
+
+plt.show()
+
+#         1. Gráfico de barras com a participação por dia da semana e por dia do mês
 #         2. Gráfico de pontos das notas por dia da semana.
 
 # 4. Salve um arquivo csv com as notas dos 100 melhores alunos ordenados da melhor nota para a pior.
