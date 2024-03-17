0cf04e82dd188207dbb29547b0198e955f7925aa
Leonardo Cardoso <lcard00@outlook.com>
2024-03-17 12:28:18 -0300

    Add pandas exercicios parte 3 e 4

--- python_para_engenharia_de_dados/modulo_02/05_pandas/pandas_exercicios.py
+++ python_para_engenharia_de_dados/modulo_02/05_pandas/pandas_exercicios.py
@@ -11,7 +11,7 @@ age = np.random.randint(0, 101, 1000)
 
 #   2. Data: data aleatórias entre 01/01/2000 até 31/01/2000.
 start_date = "2000-01-01"
-random_dates = np.random.randint(1, 32, 1000)
+random_dates = np.random.randint(0, 31, 1000)
 dates = pd.to_datetime(random_dates, origin=start_date, unit="D").date
 
 #   3. Nota: Números decimais entre 0 e 1000.
@@ -99,9 +99,9 @@ df["weekday"] = weekday
 #   1. Tabela cruzada de participantes de cada sexo por estado.
 ct_state_sex = pd.crosstab(df["state"], df["sex"])
 
-# print(ct_state_sex)
+print(ct_state_sex)
 
-#   2. Gráfico de pizza da quantidade de aprovados por sexo e um de barras com a quantidade 
+#   2. Gráfico de pizza da quantidade de aprovados por sexo e um de barras com a quantidade
 #       de aprovados por estado
 
 mask_approved = df["approved"] == True
@@ -121,14 +121,14 @@ plt.pie(
 )
 
 plt.title(
-    label="Approved by gender", 
+    label="Approved by gender",
     fontdict={"fontsize":16}
 )
 
 plt.show()
 
 approved_by_state = df[mask_approved].groupby("state")["approved"].count()
-plt.figure(figsize=(14, 10))
+plt.figure(figsize=(12, 8))
 
 plt.barh(
     approved_by_state.index,
@@ -137,36 +137,42 @@ plt.barh(
     alpha=0.9,
     edgecolor="black",
     linewidth=0.6,
-    
+
 )
 
 plt.grid(
-    axis = "x", 
+    axis = "x",
     alpha = 0.25
 )
 
+plt.xticks(
+    np.arange(2, approved_by_state.max() + 1, 2)
+)
+
+plt.xlim(2, approved_by_state.max() + 1)
+
 plt.ylabel("State")
 
+plt.ylim(-1, len(approved_by_state))
+
 plt.title(
-    label="Approved by state", 
+    label="Approved by state",
     fontdict={"fontsize":16}
 )
 
 plt.show()
 
-
 #   3. Gráfico de pontos de uma nota por idade, colorindo por sexo.
 mask_grade_by_age = df.groupby(["age", "sex"])["grade"].mean().round(2).reset_index()
-
 mask_grade_by_age = mask_grade_by_age.rename(columns= {"grade" : "mean_grade"})
 
 plt.figure(figsize=(10, 6))
 
 sns.scatterplot(
-    data=mask_grade_by_age, 
-    x="age", 
-    y="mean_grade", 
-    hue="sex", 
+    data=mask_grade_by_age,
+    x="age",
+    y="mean_grade",
+    hue="sex",
     palette="Set1",
     alpha = 0.6
 
@@ -191,6 +197,43 @@ plt.grid(True, alpha=0.2)
 plt.show()
 
 #         1. Gráfico de barras com a participação por dia da semana e por dia do mês
+mask_date_by_weekday = df.groupby(["date", "weekday"])["age"].count().reset_index()
+mask_date_by_weekday = mask_date_by_weekday.rename(columns = {"age" : "amount"})
+
+plt.figure(figsize=(12, 9))
+
+sns.barplot(
+    data = mask_date_by_weekday,
+    x = "amount",
+    y = "date",
+    hue = "weekday",
+    hue_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    palette = "Set1",
+    alpha = 0.8,
+    edgecolor = "black",
+    linewidth = 0.6,
+    dodge = False,
+    orient = "h"
+)
+
+plt.grid(True, axis="x", alpha=0.5)
+
+plt.title("Participation by weekday and date")
+
+plt.xlim(10, 28)
+
+plt.xticks(np.arange(10, 29, 2))
+
+plt.ylim(-1, 31)
+
+plt.show()
+
 #         2. Gráfico de pontos das notas por dia da semana.
 
+
 # 4. Salve um arquivo csv com as notas dos 100 melhores alunos ordenados da melhor nota para a pior.
+mask_top100 = df.nlargest(100, "grade").reset_index(drop=True)
+# print(mask_top100.sort_values("grade", ascending=False))
+
+mask_top100.index = mask_top100.index + 1
+mask_top100.to_csv("sample_data/top100_grades.csv")
